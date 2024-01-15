7251ae44d8d42fb981489e0c66f31b879038d123
GwenZi0u0 <a0918566336@gmail.com>
2024-01-15 23:12:37 +0800

    add gotop

---
+++ src/components/GoTop.vue
@@ -0,0 +1,31 @@
+<script>
+import IconGotop from './icons/IconGotop.vue';
+
+export default {
+    methods: {
+        scrollToTop() {
+            const currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+            if (currentPosition > 0) {
+                // Smooth scroll animation
+                window.requestAnimationFrame(this.scrollToTop);
+                window.scrollTo(0, currentPosition - currentPosition / 10);
+            }
+        }
+    },
+    components: {
+        IconGotop
+    }
+}
+</script>
+<template>
+    <div id="goTop" @click="scrollToTop">
+        <IconGotop></IconGotop>
+    </div>
+</template>
+<style scoped>
+#goTop {
+    position: fixed;
+    width: 0.8rem;
+    height: 0.8rem;
+}
+</style>
