dc1b88c8143f4d093ab1d5e92d5d9a8edd272ae7
Sally513 <152357760+Sally513@users.noreply.github.com>
2024-01-16 14:02:35 +0800

    更改#app

---
+++ src/components/Nav.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    components: {
+        
+    },
+    methods: {
+        
+    },
+    
+};
+</script>
+
+<template>
+    <div class="nav">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.nav{
+    background-color: #52A038;
+    width: 100%;
+    height: 592px;
+}
+</style>
