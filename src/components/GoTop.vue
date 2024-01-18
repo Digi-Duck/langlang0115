686ea47df15a34bba9d9cfa9cf696865be646daf
GwenZi0u0 <a0918566336@gmail.com>
2024-01-18 22:55:28 +0800

    fix gotop卷軸滾動到一定的高度時再出現

--- src/components/GoTop.vue
+++ src/components/GoTop.vue
@@ -2,6 +2,11 @@
 import IconGotop from './icons/IconGotop.vue';
 
 export default {
+    data() {
+        return {
+            showGoTop: false,
+        };
+    },
     methods: {
         scrollToTop() {
             const currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
@@ -10,26 +15,49 @@ export default {
                 window.requestAnimationFrame(this.scrollToTop);
                 window.scrollTo(0, currentPosition - currentPosition / 10);
             }
-        }
+        },
+        handleScroll() {
+            const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+            console.log(scrollPosition);
+            if (scrollPosition > 500) {
+                this.showGoTop = true;
+            } else {
+                this.showGoTop = false;
+            }
+        },
+    },
+    mounted() {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeDestroy() {
+        window.removeEventListener('scroll', this.handleScroll);
     },
     components: {
-        IconGotop
-    }
-}
+        IconGotop,
+    },
+};
 </script>
 <template>
-    <div id="goTop" @click="scrollToTop">
+    <div id="goTop" @click="scrollToTop" v-if="showGoTop">
         <IconGotop></IconGotop>
     </div>
 </template>
+
 <style scoped>
 #goTop {
     display: flex;
     width: 81.257px;
     height: 80.035px;
-    z-index: 10;
+    z-index: 5;
     position: fixed;
-    right: 8rem;
-    bottom: 6rem;
+    right: 5rem;
+    bottom: 16rem;
+    cursor: pointer;
+    opacity: 0.9;
+    transform: opacity 0.3s ease;
+}
+
+#goTop:hover {
+    opacity: 1;
 }
 </style>
