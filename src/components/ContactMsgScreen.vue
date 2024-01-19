26b083f6c88d48013de45b35566c76e78f77dcd6
GwenZi0u0 <a0918566336@gmail.com>
2024-01-19 14:08:24 +0800

    add 留言區彈跳視窗切版

---
+++ src/components/ContactMsgScreen.vue
@@ -0,0 +1,99 @@
+<script>
+import IconChangeButton from './icons/IconChangeButton.vue';
+
+export default {
+    data() {
+        return {
+            mysubmit: "回到首頁",
+        }
+    },
+    methods: {
+        gohomePage() {
+            this.$router.push('/');
+        },
+        closeMsgScreen() {
+            this.$emit('close');
+        }
+    },
+    components: {
+        IconChangeButton
+    }
+}
+</script>
+<template>
+    <!-- <div class="msgScreenFrame" @click="closeMsgScreen"> -->
+    <div class="msgScreenFrame">
+        <div class="msgSuccess">
+            <div class="msgSendText">
+                <img src="../assets/Image/AboutPage2Image/message-dogpaw.svg" alt="msg">
+                <span>留言成功送出</span>
+            </div>
+            <div class="msgText">
+                <p>客服人員將依序回覆，請耐心等候</p>
+            </div>
+            <IconChangeButton :text="mysubmit" @click="gohomePage"></IconChangeButton>
+        </div>
+    </div>
+</template>
+<style scoped>
+.msgScreenFrame {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    color: var(--primary-color);
+    height: 100vh;
+    width: 100vw;
+    max-width: 100%;
+    background: rgba(255, 255, 255, 0.5);
+    position: fixed;
+    top: 0;
+    z-index: 6;
+}
+
+.msgSuccess {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 30.625rem;
+    height: 23.8125rem;
+    background-color: var(--white-color);
+    border: 2px solid var(--primary-color);
+    border-radius: 30px;
+    gap: 9px;
+}
+
+.msgSendText {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+
+.msgSendText img {
+    display: flex;
+    padding-top: 2rem;
+}
+
+.msgSendText span {
+    font-size: 2.25rem;
+    padding-top: 1.5rem;
+    text-align: center;
+    letter-spacing: 9px;
+    font-weight: bold;
+}
+
+.msgText {
+    display: flex;
+    padding: 0.2rem 0 3rem 0;
+}
+
+.msgText p {
+    text-align: center;
+    font-size: 0.8rem;
+    letter-spacing: 3px;
+}
+</style>
