da83d4281b4754d396fe00ba5c7302f165888fbd
GwenZi0u0 <a0918566336@gmail.com>
2024-01-20 11:50:37 +0800

    add 分頁檔案建立

---
+++ src/views/MemberLoginView.vue
@@ -0,0 +1,28 @@
+<script>
+import FooterPage from '@/components/FooterPage.vue';
+import NavPage from '@/components/NavPage.vue';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        NavPage,
+        FooterPage
+    },
+}
+</script>
+<template>
+    <NavPage />
+    <main></main>
+    <FooterPage />
+</template>
+<style>
+main {
+    width: 100vw;
+    max-width: 100%;
+    height: 100vh;
+}
+</style>
