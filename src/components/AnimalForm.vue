5a2eb3519e3fe951f430bc0dac3259a3de268ccb
Phoebe130 <phoebewei99@gmail.com>
2024-01-20 15:26:34 +0800

    add animal form

---
+++ src/components/AnimalForm.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    props: {
+        title: String,
+        color: String,
+    },
+    data() {
+        return {
+            FormData: {
+                name: '',
+                species: '',
+            },
+        };
+    },
+    methods: {
+        submitForm() {
+            //處理表單邏輯
+            // console.log("表單提交");
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <h2>{{ title }}</h2>
+        <div class="form-group">
+            <label for="name">姓名：</label>
+            <input type="text" id="name" v-model="formData.name" />
+        </div>
+        <div class="form-group">
+            <label for="species">品種：</label>
+            <input type="text" id="species" v-model="formData.species">
+        </div>
+
+        <button @click.prevent="submitForm">提交</button>
+    </main>
+</template>
+
+<style scoped>
+/*根據顏色區分*/
+.form-group {
+    margin-bottom: 15px;
+    background-color: bisque;
+    width: 100px;
+    height: 100px;
+}
+</style>
