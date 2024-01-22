466b6823f4b7cb5e76124519aa3461e3e1dbc462
GwenZi0u0 <a0918566336@gmail.com>
2024-01-22 14:55:57 +0800

    add 標題 含右邊線的組件

---
+++ src/components/TitleAndLine.vue
@@ -0,0 +1,38 @@
+<script>
+import IconLineAndSqure from './icons/IconLineAndSqure.vue';
+
+export default {
+    props: {
+        text: String,
+        customStyle:Object,
+    },
+    components: {
+        IconLineAndSqure
+    }
+}
+</script>
+<template>
+    <div class="contactTitleArea">
+        <div class="contactTitle" :style="customStyle">
+            <span>{{ text }}</span>
+            <IconLineAndSqure />
+        </div>
+    </div>
+</template>
+<style scoped>
+.contactTitle {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+
+    /* gap: 4rem; */
+    padding: 0 1.5rem 0 1.5rem;
+}
+
+.contactTitle span {
+    color: var(--primary-color);
+    font-size: 36px;
+    font-weight: 400;
+    letter-spacing: 9px;
+}
+</style>
---
+++ src/components/icons/IconLineAndSqure.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div class="contactLine">
+        <div class="line"></div>
+        <div class="squre"></div>
+    </div>
+</template>
+<style scoped>
+.contactLine {
+    display: flex;
+}
+
+.line {
+    width: 80rem;
+    height: 2px;
+    background-color: var(--primary-color);
+}
+
+.squre {
+    width: 10px;
+    height: 10px;
+    background-color: var(--primary-color);
+    transform: translate(0px, -3.5px) rotate(45deg);
+}
+</style>
