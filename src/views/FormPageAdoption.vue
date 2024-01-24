dd6a6aae084c9261a608e43a65ac38d252091d53
GwenZi0u0 <a0918566336@gmail.com>
2024-01-24 22:55:46 +0800

    add 認養中心表單 表格段落刪除功能

--- src/views/FormPageAdoption.vue
+++ src/views/FormPageAdoption.vue
@@ -1,5 +1,6 @@
 <script>
 import AnimalForm from '@/components/AnimalForm.vue';
+import DataSubmitted from '@/components/DataSubmitted.vue';
 import FooterPage from '@/components/FooterPage.vue';
 import NavPage from '@/components/NavPage.vue';
 
@@ -7,16 +8,42 @@ export default {
     components: {
         NavPage,
         FooterPage,
-        AnimalForm
+        AnimalForm,
+        DataSubmitted
+    },
+    // data() {
+    //     return {
+    //         // 進入畫面時 視窗關閉
+    //         showSub: false,
+    //     }
+    // },
+    // methods: {
+    //     showSubOpen() {
+    //         // 執行顯示視窗
+    //         this.showSub = true;
+    //         // 視窗開啟時scrollBar隱藏
+    //         document.body.style.overflow = 'hidden';
+    //     },
+    // },
+    mounted() {
+        // 刪除段落 "發現浪浪之時間和地點 *" >>.time-place
+        const elremove = this.$refs.animalFrame;
+        const timePlaceElement = elremove.$el.querySelector(".time-place");
+        if (timePlaceElement) {
+            timePlaceElement.remove();
+        }
     },
 }
 </script>
 <template>
     <NavPage />
     <div>
-        <AnimalForm pagename="認養中心" title="毛小孩詳細資訊" inputTitleColor="var(--primary-color)"
+        <AnimalForm ref="animalFrame" pagename="認養中心" title="毛小孩詳細資訊" inputTitleColor="var(--primary-color)"
             inputTextBorder="2px solid var(--primary-color)" svgColor="var(--primary-color)"
             uploadPicColor=" var(--primary-color)" submitBgColor="var(--primary-color)" />
+        <DataSubmitted myFillColor="var(--primary-color)" mytextColor="var(--primary-color)" submitResult="資料成功送出！"
+            submitButText="前往瀏覽頁面" myurl="/adoptionpetinformation">
+        </DataSubmitted>
     </div>
     <FooterPage />
 </template>
