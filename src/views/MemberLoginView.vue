d6b8729440416907b7f1be4fb1fff7f73c4529ee
Sally513 <152357760+Sally513@users.noreply.github.com>
2024-01-25 14:42:36 +0800

    fix 會員登入/註冊/忘記密碼切板完成

--- src/views/MemberLoginView.vue
+++ src/views/MemberLoginView.vue
@@ -1,28 +1,182 @@
 <script>
 import FooterPage from '@/components/FooterPage.vue';
 import NavPage from '@/components/NavPage.vue';
-
+import GreenButton from '@/components/GreenButton.vue';
 export default {
+    components: {
+        NavPage,
+        FooterPage,
+        GreenButton
+    },
     data() {
         return {
 
         }
     },
-    components: {
-        NavPage,
-        FooterPage
+    methods: {
+        goToSignUp() {
+            this.$router.push('/memberregistration'); //導到[會員註冊]
+        },
+        goToForget() {
+            this.$router.push('/memberpassword'); //導到[忘記密碼]
+        },
+        goToPage() {
+            this.$router.push('/members'); //導到[會員頁面]
+        },
     },
 }
 </script>
 <template>
     <NavPage />
-    <main></main>
+    <main>
+        <div class="container">
+            <div class="title">會員登入</div>
+            <div class="accountbox">
+                <div class="left">
+                    <div class="account">
+                        <label>帳號</label>
+                        <input type="text" placeholder="手機號碼 / 帳號 / 信箱">
+                    </div>
+                    <div class="account">
+                        <label>密碼</label>
+                        <input type="text">
+                    </div>
+
+                </div>
+                <div class="login">
+                    登入
+                </div>
+            </div>
+            <div class="other">
+                <span @click="goToForget">忘記密碼<img src="../assets/Image/MemberImage/Icon-forget.svg"></span>
+                <span @click="goToSignUp">註冊帳號 <img src="../assets/Image/MemberImage/Icon-signup.svg"></span>
+            </div>
+
+            <span class="social"><img src="../assets/Image/MemberImage/member-line.svg">以社群帳號繼續<img
+                    src="../assets/Image/MemberImage/member-line.svg"></span>
+            <div class="icon">
+                <img src="../assets/Image/MemberImage/member-lineapp.svg" alt="">
+                <img src="../assets/Image/MemberImage/member-email.svg" alt="">
+                <img src="../assets/Image/MemberImage/member-fb.svg" alt="">
+            </div>
+        </div>
+        <!-- 分頁 -->
+        <GreenButton class="btn" @click="goToPage">導到[會員頁面]</GreenButton>
+    </main>
     <FooterPage />
 </template>
 <style scoped>
 main {
     width: 100vw;
     max-width: 100%;
-    height: 100vh;
+    padding: 102px 530px 0;
+}
+
+.container {
+    width: 818px;
+    height: 600px;
+    background-image: url(../assets/Image/MemberImage/loginBg.svg);
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.25);
+    font-family: 'ABeeZee';
+}
+
+.title {
+    font-size: 40px;
+    letter-spacing: 10px;
+    color: var(--primary-color);
+}
+
+.accountbox {
+    display: flex;
+    align-items: center;
+    gap: 32px;
+}
+
+.account {
+    font-size: 25px;
+    letter-spacing: 6px;
+    display: flex;
+    gap: 32px;
+    align-items: center;
+    color: var(--gray-color);
+    margin: 20px 0;
+}
+
+input {
+    width: 290px;
+    height: 50px;
+    background-color: rgba(217, 217, 217, 0.5);
+    border: none;
+    border-radius: 80px;
+    text-align: center;
+    font-size: 16px;
+    letter-spacing: 4px;
+}
+
+.login {
+    width: 100px;
+    height: 100px;
+    background-color: var(--white-color);
+    border-radius: 50%;
+    border: 1px solid var(--gray-color);
+    font-size: 24px;
+    letter-spacing: 6px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+}
+
+.login:hover {
+    border: 4px solid var(--gray-color);
+}
+
+.other {
+    display: flex;
+    gap: 59px;
+    color: var(--gray-color);
+    margin-right: 35px;
+    margin-bottom: 40px;
+}
+
+.other span {
+    text-decoration: underline;
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    cursor: pointer;
+}
+
+.social {
+    width: 630px;
+    display: flex;
+    justify-content: space-between;
+    gap: 25px;
+    font-size: 20px;
+    letter-spacing: 5px;
+    margin-bottom: 44px;
+}
+
+.icon {
+    width: 452px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.icon img {
+    cursor: pointer;
+}
+
+/* 按鈕暫放 */
+.btn {
+    width: 250px;
+    height: 80px;
+    margin: 10px;
 }
 </style>
---
+++ src/views/MemberRegistration.vue
@@ -0,0 +1,146 @@
+<script>
+import FooterPage from '@/components/FooterPage.vue';
+import NavPage from '@/components/NavPage.vue';
+
+export default {
+    components: {
+        NavPage,
+        FooterPage
+    },
+    data() {
+        return {
+
+        }
+    },
+
+}
+</script>
+<template>
+    <NavPage />
+    <main>
+        <div class="container">
+            <div class="title">會員註冊</div>
+            <div class="contentbox">
+                <div>
+                    <label>姓名</label>
+                    <input type="text" placeholder="王曉明">
+                </div>
+                <div>
+                    <label>電子信箱</label>
+                    <input type="text" placeholder="請輸入您的電子信箱">
+                </div>
+                <div>
+                    <label>手機號碼</label>
+                    <input type="text" placeholder="0912321123">
+                </div>
+                <div>
+                    <label>密碼</label>
+                    <input type="text" placeholder="須設定至少8個字元">
+                </div>
+                <div>
+                    <label>確認密碼</label>
+                    <input type="text">
+                </div>
+            </div>
+            <div class="other">
+                <input type="checkbox"><span>我已閱讀，並同意遵守《隱私權政策與會員服務條款》</span>
+            </div>
+            <div class="signup">註冊</div>
+
+        </div>
+    </main>
+    <FooterPage />
+</template>
+<style scoped>
+main {
+    width: 100vw;
+    max-width: 100%;
+    padding: 102px 530px 0;
+}
+
+.container {
+    width: 900px;
+    height: 700px;
+    background-image: url(../assets/Image/MemberImage/SignUpBg.svg);
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 46px;
+    box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.25);
+    font-family: 'ABeeZee';
+}
+
+.title {
+    font-size: 40px;
+    letter-spacing: 10px;
+    color: var(--primary-color);
+}
+
+.contentbox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 18px;
+    /* margin-bottom: 42px; */
+}
+
+.contentbox div {
+    width: 426px;
+    font-size: 25px;
+    text-align: left;
+    letter-spacing: 6px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: var(--gray-color);
+}
+
+input {
+    width: 290px;
+    height: 50px;
+    background-color: rgba(217, 217, 217, 0.5);
+    border: none;
+    border-radius: 80px;
+    text-align: center;
+    font-size: 16px;
+    letter-spacing: 4px;
+}
+
+.other {
+    font-size: 14px;
+    letter-spacing: 3px;
+    color: var(--gray-color);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+}
+
+.other input {
+    width: 15px;
+    height: 15px;
+    margin-right: 12px;
+}
+
+
+.signup {
+    width: 100px;
+    height: 100px;
+    background-color: var(--white-color);
+    border-radius: 50%;
+    border: 1px solid var(--gray-color);
+    font-size: 24px;
+    letter-spacing: 6px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+}
+
+.signup:hover {
+    border: 4px solid var(--gray-color);
+}
+</style>
