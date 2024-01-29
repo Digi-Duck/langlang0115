fbb48e28d4cb82038725a48d5796d98fb782ef9a
GwenZi0u0 <a0918566336@gmail.com>
2024-01-29 22:31:04 +0800

    add 建立頁面絕對路由

--- src/views/FormPageFoundAndLost.vue
+++ src/views/FormPageFoundAndLost.vue
@@ -5,18 +5,18 @@ import FooterPage from '@/components/FooterPage.vue';
 
 
 export default {
-    data(){
-        return{
-            yourDynamicRouterVariable:"/foundanimalsinformation"
+    components: {
+        NavPage,
+        AnimalForm,
+        FooterPage
+    },
+    data() {
+        return {
+            yourDynamicRouterVariable: "/foundanimalsinformation/OAAAG1121219001"
         }
     },
-    components: {
-    NavPage,
-    AnimalForm,
-    FooterPage
-},
-    mounted(){
-         // 恢復滾軸功能（導入這頁之前，popup有隱藏滾軸的功能）
+    mounted() {
+        // 恢復滾軸功能（導入這頁之前，popup有隱藏滾軸的功能）
         document.body.style.overflow = 'auto';
 
     }
@@ -27,26 +27,14 @@ export default {
     <NavPage />
     <main>
         <div>
-            <AnimalForm pagename="失散協尋"  
-            title="撿到遺失動物" 
-            inputTitleColor="#7EA138"
-            uploadPicBorder="#7EA138" 
-            svgColor="#7EA138" 
-            uploadPicColor=" #7EA138" 
-            inputTextBorder="2px solid #7EA138" 
-                submitBgColor="#7EA138"
-                popStyle="#7EA138" 
-                confirmpopStyle="#7EA138" 
-                successSvgColor="#7EA138"
-                successtextcolor="#7EA138" 
-                successBtnbgColor="#7EA138" 
-                :targetRoute="yourDynamicRouterVariable"
-                ></AnimalForm>
+            <AnimalForm pagename="失散協尋" title="撿到遺失動物" inputTitleColor="#7EA138" uploadPicBorder="#7EA138"
+                svgColor="#7EA138" uploadPicColor=" #7EA138" inputTextBorder="2px solid #7EA138" submitBgColor="#7EA138"
+                popStyle="#7EA138" confirmpopStyle="#7EA138" successSvgColor="#7EA138" successtextcolor="#7EA138"
+                successBtnbgColor="#7EA138" :targetRoute="yourDynamicRouterVariable"></AnimalForm>
         </div>
     </main>
     <FooterPage />
 </template>
 
 
-<style>
-</style>
+<style></style>
